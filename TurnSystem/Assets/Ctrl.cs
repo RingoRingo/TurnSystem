a278329331fd87adda0ae6ae524d2dcd34e6fa85
Ringo <abc_134689@163.com>
2024-04-10 18:32:59 +0800

    写了一部分判断事件

---
+++ TurnSystem/Assets/Ctrl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl : MonoBehaviour
+{
+    //private List<ItemPlayer> playList=new List<ItemPlayer>();
+
+    private GameObject[] playList;
+    /// <summary>
+    /// 当前正在处理的玩家序列
+    /// </summary>
+    private int currectPlayerIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetAllCharacter();
+        SortPlayerBySpeed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currectPlayerIndex < playList.Length)
+        {
+            playList[currectPlayerIndex].GetComponent<ItemPlayer>().Order();
+        }
+    }
+
+
+    private void GetAllCharacter() {
+        playList = GameObject.FindGameObjectsWithTag("Player");
+        //for (int i = 0; i < playList.Length; i++)
+        //{
+        //    Debug.Log($"speed={playList[i].GetComponent<ItemPlayer>().speed}");
+        //}
+    }
+
+    /// <summary>
+    /// 根据速度将所有玩家排序
+    /// </summary>
+    private void SortPlayerBySpeed() {
+        for (int i = 0; i < playList.Length - 1; i++)
+        {
+            for (int j = i + 1; j < playList.Length; j++)
+            {
+                if (playList[i].GetComponent<ItemPlayer>().speed < playList[j].GetComponent<ItemPlayer>().speed)
+                {
+                    GameObject temp = playList[i];
+                    playList[i] = playList[j];
+                    playList[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < playList.Length; i++)
+        {
+            Debug.Log($"speed={playList[i].GetComponent<ItemPlayer>().speed}");
+        }
+    }
+
+
+}
+
+public enum Team
+{
+    TeamA,
+    TeamB,
+}
+
+public enum PlayerFightState { 
+    Idle,
+    Go,
+    Attack,
+    Back
+}
+
---
+++ TurnSystem/Assets/ItemPlayer.cs
@@ -0,0 +1,54 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlayer : MonoBehaviour
+{
+    public int HP;
+    public int ATK;
+    public int speed;
+    public Team team;
+    /// <summary>
+    /// 敌人攻击自己时站位
+    /// </summary>
+    public Transform enemyPos;
+    /// <summary>
+    /// 战斗状态
+    /// </summary>
+    private PlayerFightState playerFightState=PlayerFightState.Idle;
+    /// <summary>
+    /// 攻击的目标玩家
+    /// </summary>
+    public GameObject TargetPlayer;
+
+    void Awake() {
+        HP= Random.Range(10000, 12000);
+        ATK = Random.Range(2200, 3000);
+        speed = Random.Range(800, 1200);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Transform GetEnemyPos() {
+        return enemyPos;
+    }
+
+    public void Order() {
+        if (playerFightState==PlayerFightState.Idle)
+        {
+            transform.LookAt(TargetPlayer.GetComponent<ItemPlayer>().GetEnemyPos());
+        }
+    }
+}
+
+
